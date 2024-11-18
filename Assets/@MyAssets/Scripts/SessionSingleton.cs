c9a3d658d468fb7f642a99e70de0d4207a32d054
Beñat Ramirez Ortiz <benat.ramirez@gmail.com>
2024-11-18 16:44:08 +0100

    Cambio de escena unidireccional

---
+++ Assets/@MyAssets/Scripts/SessionSingleton.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SessionSingleton : MonoBehaviour
+{
+    public static SessionSingleton instance { get; private set; }
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        } else
+        {
+            Destroy(this);
+        }
+    }
+}
