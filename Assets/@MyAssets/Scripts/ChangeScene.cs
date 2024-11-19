5971c2b899a92b89751a8ec48427fc0131a0692b
IkerVeiga <veiga.iker@opendeusto.es>
2024-11-19 11:44:09 +0100

    temporizador para cambiar entre escenas

--- Assets/@MyAssets/Scripts/ChangeScene.cs
+++ Assets/@MyAssets/Scripts/ChangeScene.cs
@@ -9,12 +9,38 @@ using UnityEngine.XR.Management;
 
 public class ChangeScene : MonoBehaviour
 {
+<<<<<<< Updated upstream
     [SerializeField] private InputActionAsset actionAsset;
     //[SerializeField] private ARSession arSession;
     //[SerializeField] private XROrigin xrOriginAR;
     //private GameObject[] arObjects;
     // Start is called before the first frame update
     void Start()
+=======
+    public ChangeScene Instance;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            DontDestroyOnLoad(this.gameObject);
+            SceneManager.sceneLoaded += OnSceneLoaded;
+            Instance = this;
+            StartCoroutine(changeScene());
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+>>>>>>> Stashed changes
     {
         //arObjects = GameObject.FindGameObjectsWithTag("AR");
         XRGeneralSettings.Instance.Manager.InitializeLoaderSync();
@@ -22,7 +48,7 @@ public class ChangeScene : MonoBehaviour
     }
 
 
-    public void changeVRAR(InputAction.CallbackContext context)
+    public void changeVRAR()
     {
         XRGeneralSettings.Instance.Manager.StopSubsystems();
         XRGeneralSettings.Instance.Manager.DeinitializeLoader();
@@ -35,6 +61,16 @@ public class ChangeScene : MonoBehaviour
         {
             SceneManager.LoadScene("AR", LoadSceneMode.Single);
         }
+    }
+
+    IEnumerator changeScene()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(5);
+            changeVRAR();
+        }
+
     }
 }
 
