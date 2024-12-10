870c4fda38353f3e50b105b9b958ad17b055c72a
Beñat Ramirez Ortiz <benat.ramirez@gmail.com>
2024-12-10 18:08:52 +0100

    Consola

---
+++ Assets/@MyAssets/Scripts/AR/PortalFix.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalFix : MonoBehaviour
+{
+    [SerializeField] private GameObject realPortal;
+    [SerializeField] private float focusTime;
+    private float timer;
+
+    public float rayTargeted()
+    {
+        Debug.Log("Raycasted");
+        timer += Time.deltaTime;
+        if (timer >= focusTime)
+        {
+            realPortal.SetActive(true);
+            gameObject.SetActive(false);
+            realPortal.GetComponent<AudioSource>().Play();
+        }
+        return timer / focusTime;
+    }
+}
+
---
+++ Assets/@MyAssets/Scripts/AR/PortalFixRaycast.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PortalFixRaycast : MonoBehaviour
+{
+    [SerializeField] private GameObject raycastOrigin;
+    [SerializeField] private Slider slider;
+
+    private void Update()
+    {
+        if (Physics.Raycast(raycastOrigin.transform.position, raycastOrigin.transform.forward, out RaycastHit hit))
+        {
+            GameObject target = hit.collider.gameObject;
+            if (target.tag == "Portal")
+            {
+                slider.gameObject.SetActive(true);
+                slider.value = target.GetComponent<PortalFix>().rayTargeted();
+            }
+            else
+            {
+                slider.gameObject.SetActive(false);
+            }
+        }
+        else
+        {
+            slider.gameObject.SetActive(false);
+        }
+    }
+}
