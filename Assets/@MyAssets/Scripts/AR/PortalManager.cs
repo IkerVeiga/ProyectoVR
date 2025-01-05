e34ae424a4c7666184220acc4ad4c1bad8d72423
IkerVeiga <145749132+IkerVeiga@users.noreply.github.com>
2025-01-05 11:52:15 +0100

    arreglar bug

--- Assets/@MyAssets/Scripts/AR/PortalManager.cs
+++ Assets/@MyAssets/Scripts/AR/PortalManager.cs
@@ -94,6 +94,10 @@ public class PortalManager : MonoBehaviour
 
     private IEnumerator SpawnNewPortal()
     {
+        if(ARportals.Count >= 4)
+        {
+            yield break;
+        }
         yield return new WaitForSeconds(3);
         Quaternion rotation = Quaternion.identity;
         Vector3 position = new Vector3(0, 0, ARportals.Count);
@@ -136,6 +140,7 @@ public class PortalManager : MonoBehaviour
         else if (SceneManager.GetActiveScene().name == "VR")
         {
             VRportals.Clear();
+            
             SceneManager.LoadScene("AR", LoadSceneMode.Single);
         }
     }
